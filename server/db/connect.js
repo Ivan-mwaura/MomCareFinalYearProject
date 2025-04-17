4d5f76e194cbf5cf33a17a5bccff1afec7543639
Ivan-mwaura <evanjustin31@gmail.com>
2025-04-17 14:11:33 +0300

    Changed the pd dialectModule  and prepare for Vercel deployment

--- server/db/connect.js
+++ server/db/connect.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectModule: require("pg"),
   dialectOptions: {
     ssl: {
       require: true,
